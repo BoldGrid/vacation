e6535d8839e8796cc2df5ba1d4f8b678310cbb4c
Tim <dev@tim.ph>
2016-05-31 15:43:04 -0400

    add container to widget area.

--- inc/boldgrid-theme-framework-config/config.php
+++ inc/boldgrid-theme-framework-config/config.php
@@ -157,15 +157,17 @@ function boldgrid_theme_framework_config( $boldgrid_framework_configs ) {
 	 * Widgets
 	 */
 	$widget_markup['call-to-action'] = <<<HTML
-	<div class="row">
-		<div class="col-md-6"></div>
-		<div class="col-md-6">
-			<div class="call-to-action">
-				<h2>WHAT SETS US APART</h2>
-				<p>We are committed to providing the absolute best quality to each and every one of our customers. Over the last 20 years, since our start, we have continued to grow while staying true to our orginal mission — to give you, the customer, a winning service that you can rely on.</p>
-				<p class="p-button-primary">
-					<a class="button-primary" href="about-us">FIND OUT MORE <i class="fa fa-chevron-circle-right"> </i></a>
-				</p>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6"></div>
+			<div class="col-md-6">
+				<div class="call-to-action">
+					<h2>WHAT SETS US APART</h2>
+					<p>We are committed to providing the absolute best quality to each and every one of our customers. Over the last 20 years, since our start, we have continued to grow while staying true to our orginal mission — to give you, the customer, a winning service that you can rely on.</p>
+					<p class="p-button-primary">
+						<a class="button-primary" href="about-us">FIND OUT MORE <i class="fa fa-chevron-circle-right"> </i></a>
+					</p>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -184,7 +186,6 @@ HTML;
 	$boldgrid_framework_configs['widget']['sidebars']['boldgrid-widget-1']['name'] = 'Below Header';
 	$boldgrid_framework_configs['widget']['sidebars']['boldgrid-widget-2']['name'] = 'Above Header';
 
-	// Configs above will override framework defaults
 	return $boldgrid_framework_configs;
 }
 add_filter( 'boldgrid_theme_framework_config', 'boldgrid_theme_framework_config' );
@@ -197,7 +198,6 @@ function filter_logo_controls( $controls ) {
 	$controls['logo_font_size']['default'] = 42;
 	$controls['logo_text_transform']['default'] = 'lowercase';
 
-	// Controls above will override framework defaults
 	return $controls;
 }
 add_filter( 'kirki/fields', 'filter_logo_controls' );
